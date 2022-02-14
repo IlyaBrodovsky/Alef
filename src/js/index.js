4c454d0f6086ffa2141de3f2d692a13d4359057e
Илья Бродовский <ilyabrodovsky@Ilas-MacBook-Pro.local>
2022-02-14 17:50:17 +0300

    new change

--- src/js/index.js
+++ src/js/index.js
@@ -1,52 +1,47 @@
-// header 
+// header
 let lastScroll = 0;
 
-const header = document.querySelector('.header');
+const header = document.querySelector(".header");
 
-const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-const containHide = () => header.classList.contains('header__hide');
+const scrollPosition = () =>
+  window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains("header__hide");
 
-window.addEventListener('scroll', () => {
-
+window.addEventListener("scroll", () => {
   if (scrollPosition() > lastScroll && !containHide()) {
-    header.classList.add('header__hide')
+    header.classList.add("header__hide");
+  } else if (scrollPosition() < lastScroll && containHide()) {
+    header.classList.remove("header__hide");
   }
-  else if (scrollPosition() < lastScroll && containHide()) {
-    header.classList.remove('header__hide')
-  }
 
-  lastScroll = scrollPosition()
-})
-// burger 
-const burger = document.querySelector('[data-burger]');
-const navList = document.querySelector('[data-nav]');
+  lastScroll = scrollPosition();
+});
+// burger
+const burger = document.querySelector("[data-burger]");
+const navList = document.querySelector("[data-nav]");
 
-burger.addEventListener('click', () => {
-  burger.classList.toggle('burger-active');
-  
-  navList.classList.toggle('nav-visible')
-  navList.classList.toggle('nav-active')
-})
+burger.addEventListener("click", () => {
+  burger.classList.toggle("burger-active");
 
+  navList.classList.toggle("nav-visible");
+  navList.classList.toggle("nav-active");
+});
 
-
-
-// slider 
+// slider
 const swiper = new Swiper(".slider__block", {
   slidesPerView: 1,
 });
 
-const sliderNavItems = document.querySelectorAll('.slider__nav-item')
+const sliderNavItems = document.querySelectorAll(".slider__nav-item");
 
 sliderNavItems.forEach((el, index) => {
-  el.setAttribute('data-index', index)
+  el.setAttribute("data-index", index);
 
-  el.addEventListener('click', (e) => {
-      const index = parseInt(e.currentTarget.dataset.index);
-      swiper.slideTo(index)
-  })
-
-})
+  el.addEventListener("click", (e) => {
+    const index = parseInt(e.currentTarget.dataset.index);
+    swiper.slideTo(index);
+  });
+});
 
 // Select
 let select = function () {
@@ -76,66 +71,74 @@ let select = function () {
 
 select();
 
-// bag and favorite 
+// bag and favorite
 
-let plusBtn = document.querySelector('.plus-btn');
-let minusBtn = document.querySelector('.minus-btn');
-let current = document.querySelector('.current');
-let addBtn = document.querySelector('.add-btn')
-let blockGoods = document.querySelector('.block-goods')
-let favorite = document.querySelector('.favorite-btn')
+let plusBtn = document.querySelector(".plus-btn");
+let minusBtn = document.querySelector(".minus-btn");
+let current = document.querySelector(".current");
+let addBtn = document.querySelector(".add-btn");
+let blockGoods = document.querySelector(".block-goods");
+let favorite = document.querySelector(".favorite-btn");
+let snackbar = document.getElementById("snackbar");
 
-document.onclick = event => {
+document.onclick = (event) => {
+  console.log(event.target);
+  if (event.target.classList.contains("plus-btn")) {
+    plusCurrent(event.target.dataset.id);
+  }
+  if (event.target.classList.contains("minus-btn")) {
+    minusCurrent(event.target.dataset.id);
+  }
+  if (event.target.classList.contains("favorite-btn")) {
+    favoriteMessage(event.target.dataset.id);
+  }
+  if (event.target.classList.contains("add-btn")) {
+    renderCart(event.target.dataset.id);
+  }
+};
 
-  console.log(event.target)
-  if (event.target.classList.contains('plus-btn')) {
-    plusCurrent(event.target.dataset.id)
-  }
-  if (event.target.classList.contains('minus-btn')) {
-    minusCurrent(event.target.dataset.id)
-  }
-  if (event.target.classList.contains('favorite-btn')) {
-    favoriteMessage(event.target.dataset.id)
-  }
-  if (event.target.classList.contains('add-btn')) {
-    renderCart(event.target.dataset.id)
-  }
+function plusCurrent() {
+  current.innerHTML = current.value++;
 }
 
-function plusCurrent () {
-  current.innerHTML = current.value ++
+function minusCurrent() {
+  if (current.value <= 0) {
+    current = 0;
+  }
+  current.innerHTML = current.value--;
 }
 
-function minusCurrent () {
-  if (current.value <= 0) { current = 0} 
-  current.innerHTML = current.value --
+function favoriteMessage() {
+  blockGoods.innerHTML =
+    "товар артикул 213456 в количестве " +
+    current.value +
+    " добавлен в избранное";
+  myFunction();
 }
-
-function favoriteMessage () {
-  blockGoods.innerHTML = 'товар артикул 213456 в количестве ' + current.value + ' добавлен в избранное'
-  myFunction()
-}
-function renderCart (){
-  blockGoods.innerHTML = 'товар артикул 213456 в количестве ' + current.value + ' добавлен в корзину'
-  myFunction()
-
+function renderCart() {
+  blockGoods.innerHTML =
+    "товар артикул 213456 в количестве " +
+    current.value +
+    " добавлен в корзину";
+  myFunction();
 }
 
 function myFunction() {
-  let x = document.getElementById("snackbar");
-  x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  snackbar.className = "show";
+  setTimeout(function () {
+    snackbar.className = snackbar.className.replace("show", "");
+  }, 3000);
 }
-addBtn.onclick = myFunction()
+addBtn.onclick = myFunction;
 
-//validate email 
+//validate email
 
 function ValidMail() {
   let re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
-  let myMail = document.querySelector('.footer__input').value;
+  let myMail = document.querySelector(".footer__input").value;
   let valid = re.test(myMail);
-  if (valid) output = 'Адрес эл. почты введен правильно!';
-  else output = 'Адрес электронной почты введен неправильно!';
-  document.getElementById('message').innerHTML = output;
+  if (valid) output = "Адрес эл. почты введен правильно!";
+  else output = "Адрес электронной почты введен неправильно!";
+  document.getElementById("message").innerHTML = output;
   return valid;
 }
